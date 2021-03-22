5718cfb04d0a0ed15ef3d8d21ad8251d8f6c7ec6
mgormana <31893852+mgormana@users.noreply.github.com>
2021-03-22 14:59:45 -0300

    Update susceptibles.R

--- susceptibles.R
+++ susceptibles.R
@@ -5,57 +5,96 @@ library(ggplot2)
 library(dplyr)
 library(matlib)
 
-a1 = 0.02
-a2 = 0.5
-a3 = 0.9
-b = 0.975
-e = 0.4
-d = 2/18.5
-g = 0.061
 
-h = 0.1
-S = 100
-
-param1 <- c(a = 0.02, b = 0.975, e = 0.4, d = 2/18.5, g = 0.061)
-tf <- 30
-times <- seq(0, tf, by = 1)
-t0 <- c(S = 100, L = 0, I = 0, D = 0, U = 0, R = 0, F = 0)
-
+#new model
 totalPop <- function(t, x, parms = NULL) {
+  with(as.list(c(x, parms)), {
   dS = -h*(I+U)*S
   dL = h*(I+U)*S - (e*L)
   dI = -(e*I) + (e*L)
-  dD = (a*e)*I + b*d*U - g*d*D - (1-g)*d*D
-  dU = (1-a)*e*I - b*d*U - (1-b)*d*U
+  dD = (a1*e)*I + b*d*U - g*d*D - (1-g)*d*D
+  dU = (1-a1)*e*I - b*d*U - (1-b)*d*U
   dR = (1-g)*d*D + (1-b)*d*U
   dF = g*d*D
-  list(c(dS, dL, dI, dD, dR, dF))
+  list(c(dS, dL, dI, dD, dR, dU, dF))
+ })
 }
 
-#transition matrix
-V <-matrix(c(
-  -e, 0, 0, 0,
-  e, -e, 0, 0,
-  0, a1*e, -d, b*d,
-  0, (1-a1)*e, 0, -d),
+params1a = c(a1=0.02, b= 0.975, e=0.4, d = 0.12, g= 0.061, h = 0.2)
+params1b = c(a1=0.5, b= 0.975, e=0.4, d = 0.12, g= 0.061, h = 0.2)
+params1c = c(a1 = 0.9, b= 0.975, e = 0.4, d = 0.12, g = 0.061, h = 0.2)
+tf <- 30
+times <- seq(0,tf, by = 1)
+t0.1 <- c(S = 1, L = 0, I = 10, D = 0, U = 10, R = 300, F = 5)
+
+
+#test the 1 susceptible within first 2
+susceptODE1a <- ode(y = t0.1, times, func = totalPop, parms = params1a)
+susceptODE1a.df <- melt(as.data.frame(susceptODE1a), id = "time")
+susceptODE1a.df <- rename(susceptODE1a.df, state = variable, scenarioA = value)
+
+#test the 1 susceptible within first 5 days
+susceptODE1b <- ode(y=t0.1, times, func = totalPop, parms = params1b)
+susceptODE1b.df <- melt(as.data.frame(susceptODE1b), id = "time")
+susceptODE1b.df <- rename(susceptODE1b.df, state = variable, scenarioB = value)
+
+#test the 1 susceiptible within days 5-10
+susceptODE1c <- ode(y=t0.1, times, func = totalPop, parms = params1c)
+susceptODE1c.df <- melt(as.data.frame(susceptODE1c), id = "time")
+susceptODE1c.df <- rename(susceptODE1c.df, state = variable, scenarioC = value)
+
+#merge to one data frame
+susceptODE1.df <- merge(susceptODE1a.df, susceptODE1b.df, by = c("time", "state"))
+susceptODE1.df <- merge(susceptODE1.df, susceptODE1c.df, by = c("time", "state"))
+
+#plotting
+ggplot(susceptODE1.df, aes(x=time, y = scenarioA, color = state)) + 
+  geom_line()
+
+ggplot(susceptODE1.df, aes(x=time, y = scenarioB, color = state)) + 
+  geom_line()
+
+ggplot(susceptODE1.df, aes(x=time, y = scenarioC, color = state)) + 
+  geom_line()
+
+#t0.2 <- c(new initial conditions given by output of ODE)
+susceptODE2a <- ode(y = t0.2, times, func = totalPop, parms = params2a)
+susceptODE2a.df <- melt(as.data.frame(susceptODE2a), id = "time")
+susceptODE2a.df <- rename(susceptODE2a.df, state = variable, scenarioA = value)
+
+
+##Finding appropriate h value using the transition matrix V
+a = 0.02
+b = 0.975
+d = 0.12
+e = 0.4
+g = 0.061
+
+V <- matrix(c(
+  -e,       0,  0,    0,
+  e,      -e,  0,    0,
+  0,     a*e, -d,  b*d,
+  0, (1-a)*e,  0,   -d),
   nrow = 4, ncol = 4, byrow = TRUE)
 
-w = matrix(c(1, 0, 0, 0), ncol =1, byrow = FALSE)
-beta = matrix(c(0, h, 0, h), nrow =1, byrow = TRUE)
-Z = (w %*% beta)*S
-x = matrix(c(L, I, D, U), ncol = 1, byrow = FALSE)
+# V <- matrix(c(
+#   -e, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+#   e, -e, 0, 0, 0, 0, 0, 0, 0, 0,
+#   0, e, -e, 0, 0, 0, 0, 0, 0, 0,
+#   0, 0, (a*e), -d, 0, 0, 0, 0, 0,0, 
+#   0, 0, 0, d, -d, b*d, 0, 0, 0, 0,
+#   0, 0, (1-a)*e, 0, 0, -d, 0, 0, 0,0, 
+#   0, 0, 0, 0, 0, (1-b)*d, -d, 0, 0, 0,
+#   0, 0, 0, 0, (1-g)*d, 0, 0, 0, 0, 0,
+#   0, 0, 0, 0, 0, 0, d, 0, 0, 0,
+#   0, 0, 0, 0, g*d, 0, 0, 0, 0,0), 
+#   nrow = 10, ncol = 10, byrow = TRUE)
 
-susceptibles <- function(t, parms = NULL) {
-  with(as.list(c(x, parms)))
-  dS = -h*(I+U)*S
-  dx = Z*x + V*x
-  list(c(dS, dx))
-}
+inverseV <- solve(-V)
 
-#says 'unused argument (parms)' ?
-susceptODE <- ode(y = t0, times, func = susceptibles, parms = param1)
-susceptODE.df <- as.data.frame(susceptODE)
+h = 0.2
+w = matrix(c(1, 0, 0, 0), nrow = 4, ncol = 1, byrow = FALSE)
+j = matrix(c(0, h, 0, h), nrow = 1, ncol = 4, byrow = TRUE)
+S = 1
 
-#says V is singular
-inverseV <- solve(V)
-Ro = beta %*% inverseV %*% w %*% S
+Ro = j %*% inverseV %*% w %*% S
